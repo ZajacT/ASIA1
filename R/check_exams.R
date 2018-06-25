5e89dcd80168cac67148c8230ddce77c1ee3319c
TZ <zajac@ZajMac.local>
2018-06-25 16:10:25 +0200

    Zmieniłem funkcje check_exams i check_results w duchu przeróbek w check_registrations

--- R/check_exams.R
+++ R/check_exams.R
@@ -1,35 +1,31 @@
 #' @title Checks if data on exams are complete and correct
-#' @description We should write something here
-#' @param registrations optionally path to the file with data on exams
-#' @return if errors are detected: data frame with added columns indicating which observations should be fixed
-#' if data are correct (corrected with the function) a data frame ready for further processing.
+#' @description Function checks if data on exams are complete and
+#' correct. If no critical eroors are detected, functions gives user a choice
+#' whether to correct detected minor issues detected in the data, or to add
+#' columns indicating which observations should be fixed. Finally it returns
+#' a data frame with corrected data or columns indicating problems.
+#' @param exams data frame with data regarding exams
+#' @return data frame
 #' @importFrom dplyr mutate if_else filter
 check_exams <- function(exams = exams) {
   #-----------------------------------------------------------------------------
   #|-> checks whether the file contains all the required variables (critical errors)
   #-----------------------------------------------------------------------------
 
-  if (!("pesel" %in% names(exams))) {
-    stop(paste0("Zmienna 'pesel' nie występuje w danych o wynikach egzaminów"))
-  }
-
-  if (!("egzamin" %in% names(exams))) {
-    stop(paste0("Zmienna 'egzamin' nie występuje w danych o wynikach egzaminów"))
-  }
-  
-  if (!("wynik_p" %in% names(exams))) {
-    stop(paste0("Zmienna 'wynik_p' nie występuje w danych o punktach rekrutacyjnych"))
-  }
-  
-  if (!("wynik_r" %in% names(exams))) {
-    stop(paste0("Zmienna 'wynik_r' nie występuje w danych o punktach rekrutacyjnych"))
+  reqVarNames <- c("pesel", "egzamin", "wynik_p","wynik_r")
+  missVarNames  <- setdiff(reqVarNames, names(exams))
+  if (length(missVarNames) > 0) {
+    stop(paste0("W danych o wynikach egzaminów ",
+                ifelse(length(missVarNames) > 1, "zmiennych", "zmiennej"), ": '",
+                paste0(missVarNames, collapse = "', '"),
+                "'."), call. = FALSE)
   }
 
   #-----------------------------------------------------------------------------
   #|-> checks whether variables contain any illegal values and offers fixes
   #-----------------------------------------------------------------------------
   
-  if(sum(!grepl("^M_",exams$egzamin))>0) {
+  if(!all(grepl("^M_",exams$egzamin))) {
     switch(menu(c("Dodaj do zbioru kolumnę 'blad_egzamin' wskazującą obserwacje wymagające poprawy ",
                   "Usuń ze zbioru wszystkie wyniki egzaminów, których opis nie zaczyna się od 'M_'"),
                 title = "Nie wszystkie opisy egzaminów w zmiennej 'egzamin' zaczynają się od 'M_'"),
--- R/check_results.R
+++ R/check_results.R
@@ -1,31 +1,33 @@
 #' @title Checks if data on scores are complete and correct
-#' @description We should write something here
-#' @param registrations optionally path to the file with data on scores
+#' @description Function checks if data on scores are complete and
+#' correct. If no critical eroors are detected, functions gives user a choice
+#' whether to correct detected minor issues detected in the data, or to add
+#' columns indicating which observations should be fixed. Finally it returns
+#' a data frame with corrected data or columns indicating problems.
+#' @param scores optionally path to the file with data on scores
 #' @return if errors are detected: data frame with added columns indicating which observations should be fixed
 #' if data are correct (corrected with the function) a data frame ready for further processing.
 #' @importFrom dplyr mutate if_else filter
+#' @export
 check_scores <- function(scores = scores) {
   #-----------------------------------------------------------------------------
   #|-> checks whether the file contains all the required variables (critical errors)
   #-----------------------------------------------------------------------------
 
-  if (!("pesel" %in% names(scores))) {
-    stop(paste0("Zmienna 'pesel' nie występuje w danych o punktach rekrutacyjnych"))
-  }
-
-  if (!("studia" %in% names(scores))) {
-    stop(paste0("Zmienna 'studia' nie występuje w danych o punktach rekrutacyjnych"))
+  reqVarNames <- c("pesel", "studia", "wynik")
+  missVarNames  <- setdiff(reqVarNames, names(scores))
+  if (length(missVarNames) > 0) {
+    stop(paste0("W danych o punktach rekrutacyjnych brakuje ",
+                ifelse(length(missVarNames) > 1, "zmiennych", "zmiennej"), ": '",
+                paste0(missVarNames, collapse = "', '"),
+                "'."), call. = FALSE)
   }
   
-  if (!("wynik" %in% names(scores))) {
-    stop(paste0("Zmienna 'wynik' nie występuje w danych o punktach rekrutacyjnych"))
-  }
-
   #-----------------------------------------------------------------------------
   #|-> checks whether variables contain any illegal values and offers fixes
   #-----------------------------------------------------------------------------
   
-  if(sum(is.na(scores$wynik) | grepl("^-",scores$wynik))>0) {
+  if(any(is.na(scores$wynik) | grepl("^-",scores$wynik))) {
     switch(menu(c("Dodaj do zbioru kolumnę 'blad_wynik' wskazującą obserwacje wymagające poprawy ",
                   "Zamień wszystkie wartości inne niż liczby równe lub większe od 0 na braki danych"),
                 title = "Zmienna 'wynik' nie może przyjmować wartości mniejszych niż 0"),
@@ -35,6 +37,6 @@ check_scores <- function(scores = scores) {
              mutate(wynik = if_else(is.na(wynik) | grepl("^-",wynik) < 0, wynik, NA))
     )
   }
-  
+
   return(scores)
 }
